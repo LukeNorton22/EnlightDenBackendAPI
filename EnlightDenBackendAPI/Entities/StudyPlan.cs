881af09fc676f2b2e79700db4153d83da938fb46
VannaMoore <vannamoore@gmail.com>
2024-10-03 16:38:21 -0500

    Started StudyPlan Controller

--- EnlightDenBackendAPI/Entities/StudyPlan.cs
+++ EnlightDenBackendAPI/Entities/StudyPlan.cs
@@ -13,3 +13,40 @@ public class StudyPlan
     public Guid UserId { get; set; }
     public User? User { get; set; }
 }
+
+public class  CreateStudyPlanDto
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+
+}
+
+public class GetStudyPlanDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+}
+
+public class  UpdateStudyPlanDto    
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+}
