2ded9a5caa94a142d0fc2bb8a680dec7db247c65
VannaMoore <vannamoore@gmail.com>
2024-10-13 12:56:48 -0500

    Just study plan controller and entity completed without ongoing work from Question controller.

--- EnlightDenBackendAPI/Entities/StudyPlan.cs
+++ EnlightDenBackendAPI/Entities/StudyPlan.cs
@@ -5,11 +5,45 @@ public class StudyPlan
     public Guid Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
-
     public int Day { get; set; }
     public int Month { get; set; }
     public long StartTime { get; set; }
     public long EndTime { get; set; }
     public required string UserId { get; set; }
     public ApplicationUser? User { get; set; }
+}
+
+public class CreateStudyPlanDto
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+    public required string UserId { get; set; }
+}
+
+public class GetStudyPlanDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+    public required string UserId { get; set; }
+    public User? User { get; set; }
+}
+
+public class UpdateStudyPlanDto
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
 }
