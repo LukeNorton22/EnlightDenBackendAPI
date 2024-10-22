afb46e55f368d648f2ddc4d2f4dcc7738f058287
VannaMoore <vannamoore@gmail.com>
2024-10-22 15:52:59 -0500

    Module and ModuleSubtopic entities started

---
+++ EnlightDenBackendAPI/Entities/Module.cs
@@ -0,0 +1,15 @@
+﻿namespace EnlightDenBackendAPI.Entities
+{
+    public class Module
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public List<ModuleTopic>? Topics { get; set; } = new List<ModuleTopic>();
+        public List<Guid> ModuleTopicIds { get; set; } = new List<Guid>();
+        public Class? Class { get; set; }
+        public Guid ClassId { get; set; }
+        public ApplicationUser? User { get; set; }
+        public required string UserId { get; set; }
+        public Guid NoteId { get; set; }
+    }
+}
---
+++ EnlightDenBackendAPI/Entities/ModuleSubtopic.cs
@@ -0,0 +1,10 @@
+﻿namespace EnlightDenBackendAPI.Entities
+{
+    public class ModuleTopic
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public Module? Module { get; set; }
+        public Guid ModuleId { get; set; }
+    }
+}
